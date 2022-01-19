05c95af6d8b3c6d577341d39ba11ce7a7fa0d342
kumartdp <42109619+kumartdp@users.noreply.github.com>
2022-01-19 16:35:41 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,6 +10,13 @@ def helloworld(){
 
 def addition() {
   
+   def x = 5; 
+      def y = 10; 
+      def z = 8; 
+		
+      //Performing addition of 2 operands 
+      println(x+y); 
+  
    echo "helloworld"
   
 }
