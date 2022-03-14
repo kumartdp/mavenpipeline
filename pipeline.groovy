083885140ac8abaa6e8ed84831dd82c2ddb9aae0
kumartdp <42109619+kumartdp@users.noreply.github.com>
2022-03-14 20:27:58 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,29 +1,8 @@
 
-
-
-def helloworld(){
-  
-  echo "helloworld"
-  
-  
-}
-
-def addition() {
-  
-   def x = 5; 
-      def y = 10; 
-      def z = 8; 
-		
-      //Performing addition of 2 operands 
-      println(x+y); 
-  
-   echo "helloworld"
-  
-}
 def pomversion() 
 {
 	
-		node('jenkins-master')
+node('jenkins-master')
 	{
 			stage('extract the pom version') 
 		{
@@ -40,87 +19,10 @@ def pomversion()
 	echo $result
 
 		}
-		}
 	}
-			
-			
-				
-		     
-	
-	
-
-	
-/*
-
-def build() {
-	
-	def registry = "saikumarzemoso/mavenimage"
-    def   registryCredential = 'dockerid'
-	
-	
-				 
-	
-				 
-	
-	 node('jenkins-master') {
-		 
-		
-		 
-		         stage("checkout") {
-           
-                
-                git  url:' https://github.com/paulczar/spring-helloworld.git'
-		sh "ls -l"
-		}
-        
-        stage("build") {
-            
-           
-            
-                sh " mvn clean install"
-            
-           }
-        
-         stage("test") {
-            
-           
-            
-                sh " mvn test"
-            
-            
-        }
-		 
- stage('Building image') {
-      
-          dockerImage = docker.build registry + ":"+ VERSION+".$BUILD_NUMBER"
-	
-        
-    }
-    stage('Deploy Image') {
-  
-          docker.withRegistry( '', registryCredential ) {
-            dockerImage.push()
-          }
-        
-        
-	 }
-		 
-	 
-  
-   echo "helloworld"
-  
-  
-  
 }
+			
 
-*/
-	
-def deployimage(){
-  
-   echo "helloworld"
-  
-
-}
 
 return this
 
