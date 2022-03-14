d980679c4febaefc4dccd5bd32159b2bef0dc25c
kumartdp <42109619+kumartdp@users.noreply.github.com>
2022-03-14 22:50:46 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,15 +9,26 @@ node('jenkins-master')
 	{
 			
 	git  url:' https://github.com/paulczar/spring-helloworld.git'
-	echo "okkk"	
-	sh 'groupId=readMavenPom().getGroupId()'
-	sh 'artifactid=readMavenPom().getArtifactId()'
-	echo "okkk2"
-	sh 'find="." '
-	sh 'replace="/" '
+	echo "okkk"
+	sh '''
+	
+	str="com.ge.avitas.gateway"
+	find="."
+	replace="/"
+	str2="apigateway-svc"
+	result=${str//$find/$replace}/${str2}
+	echo $result
+	export res=${result} '''
+	
+		
+	 // sh 'groupId=readMavenPom().getGroupId()'
+	// sh 'artifactid=readMavenPom().getArtifactId()'
+	// echo "okkk2"
+	// sh 'find="." '
+	// sh 'replace="/" '
 				
-	sh 'result="${$groupId//$find/$replace}/$artifactid" '
-	sh 'echo $result'
+	// sh 'result="${$groupId//$find/$replace}/$artifactid" '
+	// sh 'echo $result'
 
 		}
 	}
