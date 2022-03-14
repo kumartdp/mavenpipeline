8b9ad8cc995f02265095d283cf3c380767f780dd
kumartdp <42109619+kumartdp@users.noreply.github.com>
2022-03-14 22:43:18 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -11,12 +11,12 @@ node('jenkins-master')
 	git  url:' https://github.com/paulczar/spring-helloworld.git'
 		
 	sh 'groupId=readMavenPom().getGroupId()'
-	sh(''artifactid=readMavenPom().getArtifactId()'')
-	sh(''find="." '')
-	sh(''replace="/" '')
+	sh 'artifactid=readMavenPom().getArtifactId()'
+	sh 'find="." '
+	sh 'replace="/" '
 				
-	sh(''result="${$groupId//$find/$replace}/$artifactid" '')
-	sh(''echo $result)
+	sh 'result="${$groupId//$find/$replace}/$artifactid" '
+	sh 'echo $result'
 
 		}
 	}
