fbaea8c5be19a172bcf85272a9cd1c2753cd5478
kumartdp <42109619+kumartdp@users.noreply.github.com>
2022-03-15 14:48:01 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,6 +9,22 @@ node('jenkins-master')
 	{
 			
 	git  url:' https://github.com/paulczar/spring-helloworld.git'
+	pom="pom.xml"
+	groupId=$(echo "cat //*[local-name()='project']/*[local-name()='groupId']" | xmllint --shell $pom | sed '/^\/ >/d' | sed 's/<[^>]*.//g')
+	artifactId=$(echo "cat //*[local-name()='project']/*[local-name()='artifactId']" | xmllint --shell $pom | sed '/^\/ >/d' | sed 's/<[^>]*.//g')
+	version=$(echo "cat //*[local-name()='project']/*[local-name()='version']" | xmllint --shell $pom | sed '/^\/ >/d' | sed 's/<[^>]*.//g')
+	echo "ok1"
+	echo $groupId
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	echo "okkk"
 	env.groupId=readMavenPom().getGroupId()
 	env.artifactId=readMavenPom().getArtifactId()
